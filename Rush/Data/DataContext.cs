63c1facdf9009760b35ba673fccf9cd5b2650f59
SteveLorde <mostafa.maher98@gmail.com>
2024-03-12 00:04:37 +0200

    added second JS-based download function, removed Storage folder from being tracked in git

--- Rush/Data/DataContext.cs
+++ Rush/Data/DataContext.cs
@@ -14,7 +14,7 @@ public class DataContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Item>().HasData(
-            new Item {Id = Guid.Parse(""), Name = "Call Of Duty Black Ops 2", imagecover = "cover.png", Filesname = "CODBO2.rar", LastUpdated = DateOnly.FromDateTime(DateTime.Today)}
+            new Item {Id = Guid.Parse("04d98ca9-7e8b-442f-b768-0dd727ff394e"), Name = "Call Of Duty Black Ops 2", imagecover = "cover.png", Filesname = "CODBO2.rar", LastUpdated = DateOnly.FromDateTime(DateTime.Today)}
         );
     }
     
--- Rush/wwwroot/js/ViewDownloadService.js
+++ Rush/wwwroot/js/ViewDownloadService.js
@@ -3,3 +3,8 @@ export async function DownloadItem(itemid) {
     var url = `/Index?handler=OnGetDownloadItem&itemid=${itemid}`;
     await axios.get(url)
 }
+
+export function DownloadItemSecondMethod(itemid) {
+    var url = `/Index?handler=OnGetDownloadItem&itemid=${itemid}`;
+    window.location = url
+}
