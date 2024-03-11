aeb183ec015ba3d0bf2dedbf45e9b45ed6195ae3
SteveLorde <mostafa.maher98@gmail.com>
2024-03-11 10:18:57 +0200

    installed PostCSS processor and Tailwind, cleaned out Bootstrap

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx,cshtml}",
+    "./Rush/**/*.{js,ts,jsx,tsx,cshtml}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
